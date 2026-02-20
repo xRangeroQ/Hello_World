31fd395e5c28f5d84cc9096f580f8637cd08b8f3
xRangeroQ <82023366+xRangeroQ@users.noreply.github.com>
2026-02-20 09:52:41 +0300

    Add Hello World application in hello.cs

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace HelloWorldApp
+{
+  class MainClass
+  {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      Console.ReadKey();
+    }
+  }
+}
